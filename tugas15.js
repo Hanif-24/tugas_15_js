5580ae431d8c62f8266ea9e87eb76958940bd911
Hanif-Niomic <m.alihanif249@gmail.com>
2022-04-04 21:37:38 +0700

    tugas java
    tugas java

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+function segitiga(){
+  var cm = ' cm';
+  var ab = 8;
+  var bc = 6;
+  var ac = Math.sqrt(Math.pow(ab,2)+Math.pow(bc,2)) + cm ;
+  console.log('Nilai AB :',ab);
+  console.log('Nilai BC :',bc);
+  console.log("Panjang Sisi AC Pada Segitiga Tersebut Adalah : ",ac );
+}
+
+segitiga()
